c9e32e2448984ce5e2865c35864d8a39739667bd
Mauro Vieira <mauro.vieira@namecheap.com>
2018-02-22 17:06:09 +0000

    - Updated validation.

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,12 +1,12 @@
 import {UI} from '../core/ui';
 
 export class App {
-    private ball: HTMLElement;   // trigger for the color button element
-    private triggerColor: HTMLElement;   // trigger for the color button element
-    private triggerLeft: HTMLElement;   // trigger for the left animation button element
-    private triggerTop: HTMLElement;   // trigger for the top animation button element
-    private triggerDoubleSize: HTMLElement;   // trigger for the double size button element
-    private triggerResetSize: HTMLElement;   // trigger for the reset size button element
+    private ball: HTMLElement;              // trigger for the color button element
+    private triggerColor: HTMLElement;      // trigger for the color button element
+    private triggerLeft: HTMLElement;       // trigger for the left animation button element
+    private triggerTop: HTMLElement;        // trigger for the top animation button element
+    private triggerDoubleSize: HTMLElement; // trigger for the double size button element
+    private triggerResetSize: HTMLElement;  // trigger for the reset size button element
 
 
     public static readonly CSS_CLASSES: any = {
@@ -37,11 +37,7 @@ export class App {
         this.triggerLeft = document.querySelector('.' + App.CSS_CLASSES.TRIGGER_LEFT);
         this.triggerDoubleSize = document.querySelector('.' + App.CSS_CLASSES.TRIGGER_DOUBLE_SIZE);
         this.triggerResetSize = document.querySelector('.' + App.CSS_CLASSES.TRIGGER_RESET_SIZE);
-        this.registerEventHandler();
-    }
-
-    public destroy(): void {
-        this.deRegisterEventHandler();
+        this.addEventListeners();
     }
 
     public handlers() {
@@ -51,21 +47,20 @@ export class App {
                 callback: this.onClick
             }
         };
-        // init method
     }
 
-    public registerEventHandler(): void {
+    public addEventListeners(): void {
        document.body.addEventListener(this.handlers().click.event, this.handlers().click.callback);
     }
 
-    public deRegisterEventHandler(): void {
+    public removeEventListeners(): void {
        document.body.removeEventListener(this.handlers().click.event, this.handlers().click.callback);
     }
 
     /**
-    * onClick method
-    *
-    */
+     * On dispatch event click
+     * @type {[type]}
+     */
     private onClick = (ev: Event) => {
         const propStyle: any = {};
         switch (ev.target) {
@@ -85,7 +80,7 @@ export class App {
                 break;
             case this.triggerResetSize:
                 const propStyleReset: any = {};
-                this.triggerResetSize.classList.remove('ncpp-is-visible');
+                this.triggerResetSize.classList.remove('app-is-visible');
                 propStyleReset[this.cssVariables.DISPLAY] = 'none';
                 UI.changeProperty(this.triggerResetSize, propStyleReset);
                 propStyle[this.cssVariables.BALL_ANIMATION_LEFT] = '0';
@@ -93,17 +88,18 @@ export class App {
                 propStyle[this.cssVariables.WIDTH] = '50px';
                 propStyle[this.cssVariables.HEIGHT] = '50px';
                 break;
-            default:
-                // code...
-                break;
         }
         UI.changeProperty(this.ball, propStyle);
     }
 
+    /**
+     * Check if the 'RESET BUTTON' is visible
+     * @type {[type]}
+     */
     private verifyReset = () => {
         const propStyle: any = {};
-        if (!this.triggerResetSize.classList.contains('ncpp-is-visible')) {
-                this.triggerResetSize.classList.add('ncpp-is-visible');
+        if (!this.triggerResetSize.classList.contains('app-is-visible')) {
+                this.triggerResetSize.classList.add('app-is-visible');
                 propStyle[this.cssVariables.DISPLAY] = 'block';
                 UI.changeProperty(this.triggerResetSize, propStyle);
         }
--- src/components/core/ui.ts
+++ src/components/core/ui.ts
@@ -12,27 +12,28 @@ export class UI {
     public static detectBrowser(): string {
         const agent = navigator.userAgent;
         let browser;
-        if ((localStorage.getItem('browser') === undefined) ||
-            (localStorage.getItem('browser') === null)) {
-            if (agent.indexOf('Chrome') > -1) {
-                browser = 'Chrome';
-            } else if (agent.indexOf('Safari') > -1) {
-                browser = 'Safari';
-            } else if (agent.indexOf('Opera') > -1) {
-                browser = 'Opera';
-            } else if (agent.indexOf('Firefox') > -1) {
-                browser = 'Firefox';
-            } else if (agent.indexOf('Edge') > -1) {
-                browser = 'Edge';
-            } else if ((navigator.userAgent.indexOf('Trident/7.0') > 0) ||
-                (/MSIE 10/i.test(navigator.userAgent)) ||
-                (/MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent))) {
-                browser = 'IE';
-            }
-            localStorage.setItem('browser', browser);
-            return browser;
+
+        if (localStorage.getItem('browser')) {
+            return localStorage.getItem('browser');
         }
-        return localStorage.getItem('browser');
+
+        if (agent.indexOf('Chrome') > -1) {
+            browser = 'Chrome';
+        } else if (agent.indexOf('Safari') > -1) {
+            browser = 'Safari';
+        } else if (agent.indexOf('Opera') > -1) {
+            browser = 'Opera';
+        } else if (agent.indexOf('Firefox') > -1) {
+            browser = 'Firefox';
+        } else if (agent.indexOf('Edge') > -1) {
+            browser = 'Edge';
+        } else if ((navigator.userAgent.indexOf('Trident/7.0') > 0) ||
+            (/MSIE 10/i.test(navigator.userAgent)) ||
+            (/MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent))) {
+            browser = 'IE';
+        }
+        localStorage.setItem('browser', browser);
+        return browser;
     }
 
     /**
@@ -42,13 +43,13 @@ export class UI {
      * @param {type}(HTMLElement, any)
      */
     public static changeProperty(element: HTMLElement, properties: any) {
+
         if (this.detectBrowser() === 'IE') {
             Object.keys(properties).forEach((key: any | {}) => {
                 const property = key.split('-');
                 const propertyName = property[property.length - 1];
-                //This enables the use of the transform property in Internet explorer
-                if (propertyName.match('translate') || propertyName.match('rotate') || propertyName.match('skew')
-                    || propertyName.match('matrix') || propertyName.match('perpective')) {
+                // This enables the use of the transform property in Internet explorer
+                if (propertyName.match(/^translate|rotate|skew|matrix|perpective$/) ) {
                     element.style.transform = propertyName + '(' + properties[key] + ')';
                     return;
                 }
